98af4e1f3539bf80301411ddff9a55322fd3133c
Miguel Maseda <mmasedatic@gmail.com>
2019-09-27 13:36:55 +0200

    Varios

--- 04_Ejemplo_MVC_JSP/src/java/com/sinensia/controladores/ControladorPersonasServlet.java
+++ 04_Ejemplo_MVC_JSP/src/java/com/sinensia/controladores/ControladorPersonasServlet.java
@@ -26,10 +26,10 @@ public class ControladorPersonasServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         
-        String pantalla = request.getParameter("botonBuscar2");
+        String pantalla = request.getParameter("botonBuscar");
         
         switch (pantalla) {
-            case ("buscar") :
+            case ("buscar1") :
                 String nombre = request.getParameter("nombre"); // name del INPUT
                 Persona p = ServicioPersona.getInstancia().getPersona(nombre);
                 request.getSession().setAttribute("resultadoBusq", p);
@@ -49,7 +49,7 @@ public class ControladorPersonasServlet extends HttpServlet {
                 }
                 if (!personasTotal.isEmpty()) {
                     request.getSession().setAttribute("resultadosBusqueda", personasTotal);
-                    request.getRequestDispatcher("resultados_busq.jsp").forward(request, response);
+                    request.getRequestDispatcher("varios_resultados.jsp").forward(request, response);
                 }
         }
         
