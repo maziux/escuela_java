bcd7f83b31a97dbcd2de6443221977492fe5b7bd
Miguel Maseda <mmasedatic@gmail.com>
2019-10-02 12:00:03 +0200

    jsp CRUD

---
+++ 05_CRUD_Vista_JSTL/src/java/sinensia/controllers/UsersController.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sinensia.controllers;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import sinensia.model.logic.UserService;
+import sinensia.modelo.User;
+import sinensia.modelo.persistence.IUserDAO;
+import sinensia.modelo.persistence.UserDAO_DerbyDB;
+
+/**
+ *
+ * @author alumno
+ */
+@WebServlet(asyncSupported = true, urlPatterns = "/users.do")
+public class UsersController extends HttpServlet {
+ 
+    UserService userSrv;
+
+    @Override
+    public void init() throws ServletException {
+        super.init(); //To change body of generated methods, choose Tools | Templates.
+        
+        IUserDAO usersDAO = new UserDAO_DerbyDB();
+        this.userSrv = new UserService(usersDAO);
+    }
+ 
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) 
+            throws ServletException, IOException {        
+        try {
+            String id = req.getParameter("id");
+            String email = req.getParameter("email");
+            String password = req.getParameter("password");
+            String name = req.getParameter("name");
+            String age = req.getParameter("age");
+
+            String method = req.getParameter("method");
+            if ("Delete".equals(method)) {
+                userSrv.remove(Integer.parseInt(id));
+            } else if ("Update".equals(method)) {
+                User updateUser = new User(email,password,name,Integer.parseInt(age));
+                updateUser.setId(Integer.parseInt(id));
+                userSrv.update(updateUser);
+            } else {
+                User newUser = userSrv.create(email, password, name, age);
+                req.setAttribute("user", newUser);
+            }
+        } catch (Exception ex) {
+            Logger.getLogger(UsersController.class.getName()).log(Level.SEVERE, null, ex);
+            req.setAttribute("errorMessage", ex.getMessage());
+        } finally {
+            req.getRequestDispatcher("result.jsp").forward(req, resp);
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try {
+            List<User> listUsers = userSrv.getAll();
+            req.setAttribute("usersList", listUsers);
+            req.getRequestDispatcher("listUsers.jsp").forward(req, resp);
+        } catch (Exception ex) {
+             Logger.getLogger(UsersController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }    
+    
+}
---
+++ 05_CRUD_Vista_JSTL/web/listUsers.jsp
@@ -0,0 +1,45 @@
+<%-- 
+    Document   : listUsers
+    Created on : 01-oct-2019, 16:47:47
+    Author     : alumno
+--%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@page import="sinensia.modelo.User"%>
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<jsp:useBean id="usersList" 
+             type="java.util.ArrayList<User>" scope="request">
+    <jsp:getProperty property="*" name="usersList"></jsp:getProperty>
+</jsp:useBean>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Users list</title>
+    </head>
+    <body>
+        <h1>Users list</h1>
+        <%-- ${} corresponde a EL (Expression Language) de JSTL --%>
+        
+        <c:forEach items="${usersList}" var="user">
+            <form action="users.do" method="post">
+                <table>
+                    <tr>
+                        <td><input id="id" name="id" 
+                                   readonly type="number" size="4" 
+                                   value="${user.id}"/></td>
+                        <td><input id="email" name="email"
+                                   type="email" value="${user.email}"/></td>
+                        <td><input id="password" name="password"
+                                   type="text" value="${user.password}"/></td>
+                        <td><input id="name" name="name"
+                                   type="text" value="${user.name}"/></td>
+                        <td><input id="age" name="age"
+                                   type="number" value="${user.age}"/></td>
+                        <td><input name="method" type="submit" value="Delete"/></td>
+                        <td><input name="method" type="submit" value="Update"/></td>
+                    </tr>
+                </table>
+            </form>
+        </c:forEach>
+    </body>
+</html>
